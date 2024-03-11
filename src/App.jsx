533f9ee4aec1a8ac450bf8989dc18aae953dc9c6
David-F-98 <u2903149@unimilitar.edu.co>
2024-03-11 16:15:05 -0500

    Rutas & Estructura Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const App = () => {
   return ( 
     <>
-      <h1>Hola </h1>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Contenedor from './elements/Contenedor.jsx';
 import WebFont from 'webfontloader';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import EditarGasto from './components/EditarGasto.jsx';
+import GastosPorCategoria from './components/GastosPorCategoria.jsx';
+import InicioDeSesion from './components/InicioDeSesion.jsx';
+import ListaDeGastos from './components/ListaDeGastos.jsx';
+import RegistroUsuarios from './components/RegistroUsuarios.jsx';
+
+
+
 WebFont.load({
   google: {
     families: ['Work Sans:400,500,700', 'sans-serif']
@@ -11,6 +21,18 @@ WebFont.load({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Contenedor>
+        <Routes>
+          <Route path='/iniciar-sesion' element={<InicioDeSesion/>}></Route>
+          <Route path='/crear-cuenta' element={<RegistroUsuarios/>}></Route>
+          <Route path='/categorias' element={<GastosPorCategoria/>}></Route>
+          <Route path='/lista' element={<ListaDeGastos/>}></Route>
+          <Route path='/editar/:id' element={<EditarGasto/>}></Route>
+          <Route path='/' element={<App/>}></Route>
+        </Routes>
+        <App /> {/*Página por defector*/}
+      </Contenedor>
+    </BrowserRouter>
   </React.StrictMode>,
 )
