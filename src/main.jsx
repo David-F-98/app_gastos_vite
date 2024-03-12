3dd8b481fbb0f7a576c00a6a0fb6e9e84cb3e2dc
David-F-98 <u2903149@unimilitar.edu.co>
2024-03-11 19:13:51 -0500

    Agregando el Favicon, el Fondo y los puntos

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,9 @@ import GastosPorCategoria from './components/GastosPorCategoria.jsx';
 import InicioDeSesion from './components/InicioDeSesion.jsx';
 import ListaDeGastos from './components/ListaDeGastos.jsx';
 import RegistroUsuarios from './components/RegistroUsuarios.jsx';
+import { Helmet } from 'react-helmet';
+import favicon from './images/logo.png';
+import Fondo from './elements/Fondo.jsx';
 
 
 
@@ -20,19 +23,26 @@ WebFont.load({
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Contenedor>
-        <Routes>
-          <Route path='/iniciar-sesion' element={<InicioDeSesion/>}></Route>
-          <Route path='/crear-cuenta' element={<RegistroUsuarios/>}></Route>
-          <Route path='/categorias' element={<GastosPorCategoria/>}></Route>
-          <Route path='/lista' element={<ListaDeGastos/>}></Route>
-          <Route path='/editar/:id' element={<EditarGasto/>}></Route>
-          <Route path='/' element={<App/>}></Route>
-        </Routes>
-        <App /> {/*Página por defector*/}
-      </Contenedor>
-    </BrowserRouter>
-  </React.StrictMode>,
+  <>
+    <Helmet>
+      <link rel='shortcut icon' href={favicon} type='image/x-icon' />
+    </Helmet>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Contenedor>
+          <Routes>
+            <Route path='/iniciar-sesion' element={<InicioDeSesion/>}></Route>
+            <Route path='/crear-cuenta' element={<RegistroUsuarios/>}></Route>
+            <Route path='/categorias' element={<GastosPorCategoria/>}></Route>
+            <Route path='/lista' element={<ListaDeGastos/>}></Route>
+            <Route path='/editar/:id' element={<EditarGasto/>}></Route>
+            <Route path='/' element={<App/>}></Route>
+          </Routes>
+          <App />
+        </Contenedor>
+      </BrowserRouter>
+
+      <Fondo/>
+    </React.StrictMode>,
+  </>
 )
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+ 
+// Lo importo
+import svgr from "vite-plugin-svgr";
+ 
+// Lo añado a los plugins exportados
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+});
